13ca11afc1323163b51f067a1cee6c594984f7d2
Tedd Arcuri <tarcuri88@gmail.com>
2013-12-27 11:43:41 -0700

    Updated profile page and added scoped status updates to it

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rootbook::Application.routes.draw do
   root to: 'statuses#index'
 
 
+  get '/:id', to: 'profiles#show'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
