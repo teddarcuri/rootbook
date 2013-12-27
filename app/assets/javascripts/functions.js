b9f7c80c857a76af595300197d1490504d6888ca
Tedd Arcuri <tarcuri88@gmail.com>
2013-12-26 21:20:12 -0700

    Added tests for user first name, last name, and profile name

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("#toolbox-container").click(function(){
-		$("#toolbox").slideToggle();
+		$("#toolbox").fadeToggle();
 	})
 });
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,30 @@ class User < ActiveRecord::Base
   				  :first_name, :last_name, :profile_name   
   # attr_accessible :title, :body
 
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true,
+                          uniqueness: true,
+                          format: {
+                            with: /a-zA-Z0-9_-/,
+                            message: 'Must be formatted correctly.'
+                          }
+
   has_many :statuses
 
+  # Get full name variable
   def full_name
   	first_name + " " + last_name
   end
 
+  # Pull in profile image from Gravatar.com
   def gravatar_url
-
     stripped_email = email.strip
     downcased_email = stripped_email.downcase
     hash = Digest::MD5.hexdigest(downcased_email)
 
     "http://gravatar.com/avatar/#{hash}?d=retro"
-
   end
 end
